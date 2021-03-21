3d1d83866847f0eb204fc1c8955e9b77ae878f62
chiaz91 <chiayu.dev@gmail.com>
2021-03-21 19:53:22 +0800

    share apk as zip

--- Android/app/src/main/java/ntu/cz3004/controller/fragment/SettingsFragment.java
+++ Android/app/src/main/java/ntu/cz3004/controller/fragment/SettingsFragment.java
@@ -7,6 +7,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.SharedPreferences;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
@@ -14,15 +15,19 @@ import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
 import androidx.preference.Preference;
 import androidx.preference.PreferenceFragmentCompat;
 import androidx.preference.PreferenceManager;
 import androidx.preference.SwitchPreference;
 
+import java.io.File;
+
 import ntu.cz3004.controller.BuildConfig;
 import ntu.cz3004.controller.R;
 import ntu.cz3004.controller.common.Constants;
 import ntu.cz3004.controller.util.IntentBuilder;
+import ntu.cz3004.controller.util.MdpLog;
 import ntu.cz3004.controller.util.Utility;
 
 public class SettingsFragment extends PreferenceFragmentCompat implements SharedPreferences.OnSharedPreferenceChangeListener,
@@ -49,6 +54,13 @@ public class SettingsFragment extends PreferenceFragmentCompat implements Shared
         findPreference("info_version").setSummary(String.format("%s (%d)",BuildConfig.VERSION_NAME, BuildConfig.VERSION_CODE));
         findPreference("info_build").setSummary(String.format("%s (%s)",BuildConfig.BUILD_TYPE, BuildConfig.TIMESTAMP));
 
+        // share apk
+        Preference shareApkPref = findPreference("share_apk");
+        shareApkPref.setVisible(BuildConfig.BUILD_TYPE.equalsIgnoreCase("release"));
+        shareApkPref.setOnPreferenceClickListener(preference -> {
+            shareApkAsZip();
+            return true;
+        });
 
         // bluetooth functions
         btAdapter = BluetoothAdapter.getDefaultAdapter();
@@ -151,4 +163,27 @@ public class SettingsFragment extends PreferenceFragmentCompat implements Shared
             }
         }
     };
+
+    private void shareApkAsZip()  {
+        File srcFile = new File(getContext().getApplicationInfo().publicSourceDir);
+        File destFile = new File(getContext().getCacheDir(), getContext().getString(R.string.app_name)+".zip");
+
+        MdpLog.d(TAG, "Path: "+destFile.getAbsolutePath());
+
+        try{
+            Utility.zip(destFile.getAbsolutePath(), srcFile.getAbsolutePath());
+            Uri apkUri = FileProvider.getUriForFile(getContext(), BuildConfig.APPLICATION_ID+".provider", destFile);
+            MdpLog.d(TAG, "Path: "+apkUri.getPath());
+
+            Intent share = new Intent();
+            share.setAction(Intent.ACTION_SEND);
+            share.setType("application/zip");
+            share.putExtra(Intent.EXTRA_STREAM, apkUri);
+            share.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+            startActivity(Intent.createChooser(share, getString(R.string.share_app_title)));
+
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+    }
 }
